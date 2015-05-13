d1719c4e0c866a25646476865e82f5ccd87cdc27
kijowski <michal.kijowski@hotmail.com>
2015-05-13 23:53:37 +0100

    First version of init script
    Added first version of init script and modified build.cmd

--- init.fsx
+++ init.fsx
@@ -1,5 +1,3 @@
-#r @"../packages/FAKE/tools/FakeLib.dll"
-open Fake
 open System
 open System.IO
 open System.Collections.Generic
@@ -10,8 +8,16 @@ open System.Collections.Generic
 // It generates the build.fsx and generate.fsx files 
 // --------------------------------
 
-let dirsWithProjects = ["src";"progress"] 
-                       |> List.map (fun d -> directoryInfo (__SOURCE_DIRECTORY__ @@ d))
+// --------------------------------
+// helper functions brought from FAKE
+// --------------------------------
+let inline directoryInfo path = new DirectoryInfo(path)
+let inline subDirectories (dir : DirectoryInfo) = dir.GetDirectories()
+let filesInDirMatching pattern (dir : DirectoryInfo) = 
+    if dir.Exists then dir.GetFiles pattern
+    else [||]
+let inline combinePaths path1 (path2 : string) = Path.Combine(path1, path2.TrimStart [| '\\'; '/' |])
+let inline (@@) path1 path2 = combinePaths path1 path2
 
 // special funtions
 // many whom might be replaceable with FAKE functions
@@ -28,30 +34,19 @@ let move p1 p2 =
   else
     failwithf "Could not move %s to %s" p1 p2
 let localFile f = combine f __SOURCE_DIRECTORY__ 
-let buildTemplatePath = localFile "build.template" 
-let outputPath = localFile "build.fsx" 
 
 let prompt (msg:string) = 
   Console.Write(msg)
   Console.ReadLine().Trim() 
   |> function | "" -> None | s -> Some s
   |> Option.map (fun s -> s.Replace ("\"","\\\""))
-let runningOnAppveyor =
-  not <| String.IsNullOrEmpty(Environment.GetEnvironmentVariable("CI"))
-let runningOnTravis =
-  not <| String.IsNullOrEmpty(Environment.GetEnvironmentVariable("TRAVIS"))
-let inCI = runningOnAppveyor || runningOnTravis
 let promptFor friendlyName = 
-  if inCI then Some "CONTINUOUSINTEGRATION"
-  else prompt (sprintf "%s: " friendlyName)
+  prompt (sprintf "%s: " friendlyName)
 let rec promptForNoSpaces friendlyName =
   match promptFor friendlyName with
   | None -> None
   | Some s when not <| String.exists (fun c -> c = ' ') s -> Some s
   | _ -> Console.WriteLine("Sorry, spaces are disallowed"); promptForNoSpaces friendlyName
-
-failfUnlessExists buildTemplatePath "Cannot find build template file %s" 
-  (Path.GetFullPath buildTemplatePath)
 
 // User input
 let border = "#####################################################"
@@ -65,11 +60,8 @@ let print msg =
 print """
 # Project Euler Init Script
 # Please answer a few questions and we will generate
-# two files:
-#
-# build.fsx               This will be your build script
-# progress/tools/generate.fsx This script will generate your 
-#                         progress tracking site
+# solution with script files for Project Euler problems and 
+# build.fsx build script 
 #
 # NOTE: Aside from the Project Name, you may leave any 
 # of these blank, but you will need to change the defaults 
@@ -81,8 +73,6 @@ let vars = Dictionary<string,string option>()
 vars.["##ProjectName##"] <- promptForNoSpaces "Project Name (used for solution/project files)"
 vars.["##Summary##"]     <- promptFor "Summary (a short description)"
 vars.["##Author##"]      <- promptFor "Author"
-vars.["##GitHome##"]     <- promptFor "Github User or Organization"
-vars.["##GitName##"]     <- promptFor "Github Project Name (leave blank to use Project Name)"
 
 //Basic settings
 
@@ -103,19 +93,17 @@ let projectName =
 let solutionFile = localFile (projectName + ".sln")
 move templateSolutionFile solutionFile
 
-//Rename project files and directories
-dirsWithProjects 
-|> List.iter (fun pd -> 
-    // project files
-    pd
-    |> subDirectories
-    |> Array.collect (fun d -> filesInDirMatching "*.?sproj" d)
-    |> Array.iter (fun f -> f.MoveTo(f.Directory.FullName @@ (f.Name.Replace(projectTemplateName, projectName))))
-    // project directories
-    pd
-    |> subDirectories
-    |> Array.iter (fun d -> d.MoveTo(pd.FullName @@ (d.Name.Replace(projectTemplateName, projectName))))
-    )
+let dirWithProject = directoryInfo (__SOURCE_DIRECTORY__ @@ "src")
+//Rename project file
+dirWithProject 
+|> subDirectories
+|> Array.collect (fun d -> filesInDirMatching "*.?sproj" d)
+|> Array.iter (fun f -> f.MoveTo(f.Directory.FullName @@ (f.Name.Replace(projectTemplateName, projectName))))
+//Rename project directory
+dirWithProject 
+|> subDirectories
+|> Array.iter (fun d -> d.MoveTo(dirWithProject.FullName @@ (d.Name.Replace(projectTemplateName, projectName))))
+    
 
 //Now that everything is renamed, we need to update the content of some files
 let replace t r (lines:seq<string>) = 
@@ -138,8 +126,6 @@ let replaceContent file =
   |> replaceWithVarOrMsg "##Author##" "Author not set" 
   |> replaceWithVarOrMsg "##Description##" "Description not set" 
   |> replaceWithVarOrMsg "##Summary##" ""
-  |> replaceWithVarOrMsg "##Tags##" "" 
-  |> replaceWithVarOrMsg "##GitHome##" "[github-user]"
   |> overwrite file
   |> sprintf "%s updated"
 
@@ -149,31 +135,103 @@ let rec filesToReplace dir = seq {
   yield! Directory.GetFiles(dir, "*.cs")
   yield! Directory.GetFiles(dir, "*.xaml")
   yield! Directory.GetFiles(dir, "*.fsx")
-  yield! Directory.GetFiles(dir, "paket.template")
   yield! Directory.EnumerateDirectories(dir) |> Seq.collect filesToReplace
 }
 
-[solutionFile] @ (dirsWithProjects  
-    |> List.collect (fun d -> d.FullName |> filesToReplace |> List.ofSeq)) 
+[solutionFile] @ (dirWithProject.FullName |> filesToReplace |> List.ofSeq) 
 |> List.map replaceContent
 |> List.iter print
 
-//Replace tokens in build template
-let generate templatePath generatedFilePath = 
-  failfUnlessExists templatePath "Cannot find template %s" (templatePath |> Path.GetFullPath)
-  
-  let newContent = 
-    File.ReadAllLines(templatePath) |> Array.toSeq 
-    |> replace "##ProjectName##" projectName
-    |> replaceWithVarOrMsg "##Summary##" "Project has no summmary; update build.fsx"
-    |> replaceWithVarOrMsg "##Author##" "Update Author in build.fsx" 
-    |> replaceWithVarOrMsg "##GitHome##" "Update GitHome in build.fsx"
-    |> replace "##GitName##" projectName
+// Generate project
 
-  File.WriteAllLines(generatedFilePath, newContent)
-  File.Delete(templatePath)
-  print (sprintf "# Generated %s" generatedFilePath)
+// Prompt user for generation parameters
+let rec promptForNumber defNumber friendlyName =
+  match promptFor friendlyName with
+  | Some s when fst (Int32.TryParse(s)) -> Int32.Parse(s)
+  | None -> defNumber
+  | _ -> Console.WriteLine("Please enter an integer") ; promptForNumber defNumber friendlyName
 
-generate (localFile "build.template") (localFile "build.fsx")
-generate (localFile "progress/tools/generate.template") (localFile "progress/tools/generate.fsx")
+print  """
+# You can specify one of the following project structures:
+# 1. One problem per file. All files in root project directory
+# 2. One problem per file. Files grouped in solution folders (user specified folder size)
+# 3. Multiple problem per file. All files in root project directory
+# 4. Multiple problem per file. Files grouped in solution folders (user specified folder size)
+"""
+
+let problemsPerFile = promptForNumber 1 "How many problems per file (default 1):"
+let filesPerDirectory = 
+    match promptForNumber 25 "How many files per directory (default 25, set to 0 for all files in root directory):" with
+    | 0 -> None
+    | x -> Some x
+
+// Load libraries for project manipulation and Json parsing
+#r @"Microsoft.Build.dll"
+#r @"packages\FSharp.Data\lib\net40\FSharp.Data.dll"
+open Microsoft.Build.Evaluation
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+
+// Simple domain model
+type EulerProblem = {
+    Number : int
+    Title : string
+    Content : string
+    Difficulty : string
+}
+
+let parseProblems (file:string) =
+    [for problem in JsonValue.Load(file) do
+        yield {Number=problem?Number.AsInteger()
+               Title = problem?Title.AsString()
+               Content = problem?Content.AsString()
+               Difficulty=problem?Difficulty.AsString()
+               }
+    ]   
+
+let rootProjectDir = dirWithProject.FullName @@ projectName
+let rootDataDir = localFile "data" 
+let project = new Project(rootProjectDir @@ projectName @@ ".fsproj")
+let jsonFile = rootDataDir @@ "problems.json"
+let templateFile = 
+    if problemsPerFile = 1 then rootDataDir @@ "single.template"
+    else rootDataDir @@ "multiple.template"
+    
+
+let applyTemplate problem =
+    File.ReadAllLines(templateFile)
+    |> replace "@Number" (problem.Number.ToString())
+    |> replace "@Title" problem.Title
+    |> replace "@Content" problem.Content
+    |> replace "@Difficulty" problem.Difficulty
+    |> replace "@NestLevel" (if filesPerDirectory.IsSome then @"..\" else @"")    
+    |> Seq.reduce(+)
+    
+let fileName i =
+    if problemsPerFile = 1 then sprintf "Problem%3d.fsx" i
+    else sprintf "Problems%3d-%3d.fsx" (i * problemsPerFile + 1) ((i+1) * problemsPerFile) 
+
+let folderName i =
+    match filesPerDirectory with
+        | Some fpd -> rootProjectDir @@ sprintf "%3d-%3d" (i * fpd * problemsPerFile + 1) ((i+1) * fpd * problemsPerFile) 
+        | None -> rootProjectDir        
+
+let addProblem folder (fileName, content) =
+    if not (Directory.Exists(folder)) then Directory.CreateDirectory folder |> ignore            
+    File.WriteAllText(folder @@ fileName, content)
+    project.AddItem("None", fileName) |> ignore
+
+parseProblems jsonFile
+|> List.sortBy(fun prob -> prob.Number)
+|> Seq.windowed(problemsPerFile)
+|> Seq.mapi(fun i problems -> fileName i, problems)
+|> Seq.map(fun (fileName, problems) -> fileName,problems |> Seq.map(applyTemplate) |> Seq.reduce(+))
+|> Seq.windowed(defaultArg filesPerDirectory 1)
+|> Seq.mapi(fun i files -> folderName i, files)
+|> Seq.iter(fun (folder, files) -> files |> Seq.iter(addProblem folder))
+
+project.Save()
+
+
 File.Delete "init.fsx"
+
