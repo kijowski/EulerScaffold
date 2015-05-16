379870731b8ea6969b402f78bdff67b3b5e6e80c
kijowski <michal.kijowski@hotmail.com>
2015-05-16 22:55:44 +0100

    Modified helpers file
    Modified helpers file to add functions for downloading of single problem

--- helpers.fsx
+++ helpers.fsx
@@ -6,70 +6,74 @@ open System.Text.RegularExpressions
 open Microsoft.Build.Evaluation
 open System.IO
 open System.Collections.Generic
+open System.Net
  
 // domain model for project euler problem definitions
 type EulerProblem = {
-    Number : int
-    Title : string
-    Content : string
+  Number : int
+  Title : string
+  Content : string
+  Difficulty : string
+  Raw : string
 }
  
 // helper functions
 let regexRep (patt:string) (repl:string) input =
-    Regex.Replace(input, patt, repl)
+  Regex.Replace(input, patt, repl)
  
 let processTemplate template problem =
-    template
-    |> regexRep "@Number" (problem.Number.ToString())
-    |> regexRep "@Title" problem.Title
-    |> regexRep "@Content" problem.Content
+  template
+  |> regexRep "@Number" (problem.Number.ToString())
+  |> regexRep "@Title" problem.Title
+  |> regexRep "@Content" problem.Content
  
+let username = "username";
+let password = "******";
+
+let cc = CookieContainer()
+Http.Request(@"https://projecteuler.net/sign_in",
+  body = FormValues ["username", username
+                   "password", password
+                     "remember_me", "1"
+                     "sign_in","Sign+In"
+                     ], cookieContainer=cc) |> ignore
+
+let (|SimpleNode|ComplexNode|) (node:HtmlNode) =
+  if node.Elements().IsEmpty 
+  then SimpleNode (node.Name(), node.InnerText())
+  else ComplexNode (node.Name(), node.Elements())
+
+
 let downloadProblem n =
-    let rec simpleParse (node:HtmlNode) =
-        match node.Elements() |> Seq.isEmpty with
-            | true -> node.InnerText()
-            | false -> node.Elements() |> Seq.map simpleParse |> Seq.reduce(+)
- 
-    let problemPage = Http.RequestString(sprintf "https://projecteuler.net/problem=%d" n)
-    match problemPage.Contains("Problem not accessible") with
-        | false ->  
-                let problemPage = HtmlDocument.Parse(problemPage)    
-                let title = problemPage.Descendants ["h2"] |> Seq.head
-                let problemInfo = problemPage.Descendants(fun x -> x.HasId("problem_info")) |> Seq.head
-                let problemContent = problemPage.Descendants(fun x -> x.HasClass("problem_content")) |> Seq.head
-                Some( {
-                        Number = n
-                        Title = title.InnerText()
-                        Content = simpleParse problemContent
-                      })
-        | true -> None
-    
-let addProblem templateProcessor (project:Project) problemGroup eulerProblem =
-    let folder = sprintf "D:\Source\Playground\ProjectEuler\ProjectEuler\%d" problemGroup
-    if not (Directory.Exists(folder)) then Directory.CreateDirectory folder |> ignore
-    let fileName = Path.Combine(sprintf "%d" problemGroup, sprintf "Problem%d.fsx" eulerProblem.Number)
-    printfn "%s" fileName
-    File.WriteAllText(Path.Combine("D:\Source\Playground\ProjectEuler\ProjectEuler", fileName), eulerProblem |> templateProcessor)
-    project.AddItem("None", fileName) |> ignore
- 
-let downloadAll template project =
-    Seq.initInfinite (fun x->x+1) 
-    |> Seq.map(fun n -> downloadProblem n) 
-    |> Seq.takeWhile(fun problem -> problem.IsSome) 
-    |> Seq.map Option.get
-    |> Seq.groupBy(fun problem -> problem.Number/26)
-    |> Seq.iter(fun (group, problems) -> problems |> Seq.iter (addProblem template project group))
- 
-    
-let projectFilePath = @"D:\Source\Playground\ProjectEuler\ProjectEuler\ProjectEuler.fsproj"
-let templateContent = File.ReadAllText(@"D:\Source\Playground\ProjectEuler\ProjectEuler\Problem.template")
-let definitions = Dictionary<string,string>()
-definitions.Clear()
-definitions.Add("MSBuildExtensionsPath32",@"C:\Program Files (x86)\MSBuild")
-definitions.Add("VisualStudioVersion",@"12.0")
-definitions.Add("FSharpTargetsPath",@"C:\Program Files (x86)\Microsoft SDKs\F#\3.0\Framework\v4.0\Microsoft.FSharp.Targets")
- 
-let project = Project(projectFilePath,definitions,"12.0")
- 
-downloadAll (processTemplate templateContent) project
-project.Save()
+  let rec parseProblem (node:HtmlNode) = 
+    match node with
+      | SimpleNode("img",_) -> if node.TryGetAttribute("src").IsSome then
+                                 let file = @"https://projecteuler.net/" + node.Attribute("src").Value()
+                                 use client = new WebClient()
+                                 client.DownloadFile(file, @"C:\Users\michal\Downloads\ProjectScaffold-master\ProjectScaffold-master\src\ProjectEuler\" + Path.GetFileName(file))
+                                 sprintf "![alt text](%s)" (node.Attribute("src").Value())
+                               else ""
+      | SimpleNode("b",text) -> "**" + text + "**"
+      | SimpleNode(name,text) -> text
+      | ComplexNode(name,elems) -> elems |> List.map parseProblem |> List.reduce(+)
+  let getDifficulty (node:HtmlNode) = 
+    printfn "%s" (node.InnerText())
+    let reg = Regex.Match(node.InnerText(), ".*Difficulty rating\: (?<diff>\d*).*")
+    if reg.Success then reg.Groups.["diff"].Value else "unknown"
+  let problemPage = Http.RequestString(sprintf "https://projecteuler.net/problem=%d" n, cookieContainer=cc)
+  match problemPage.Contains("Problem not accessible") with
+    | false ->  
+              let problemPageParsed = HtmlDocument.Parse(problemPage)    
+              let title = problemPageParsed.Descendants ["h2"] |> Seq.head
+              let problemInfo = problemPageParsed.Descendants(fun x -> x.HasId("problem_info")) |> Seq.head
+              let problemContent = problemPageParsed.Descendants(fun x -> x.HasClass("problem_content")) |> Seq.head
+              Some( {
+                     Number = n
+                     Title = title.InnerText()
+                     Content = parseProblem problemContent
+                     Difficulty = getDifficulty problemInfo
+                     Raw = problemPage
+                    })
+    | true -> None
+
+downloadProblem 405
